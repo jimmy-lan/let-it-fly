8be2a6452114e15df124c9a3472bd5dffdfec5f4
Jimmy <jimmylanbcn@gmail.com>
2020-10-24 23:39:41 -0400

    Add tests for AppFrame

--- client/src/common/components/AppFrame.test.tsx
+++ client/src/common/components/AppFrame.test.tsx
@@ -1,2 +1,12 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
+// @ts-ignore
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { AppFrame } from "./AppFrame";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without exploding", () => {
+  const wrapper = shallow(<AppFrame />);
+  expect(wrapper).toBeTruthy();
+});
