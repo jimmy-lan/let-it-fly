2a99d150aeddeebf569792f9d55fa5c0f87fcfbc
michael <skyjoy.liu@mail.utoronto.ca>
2020-10-29 21:08:50 -0700

    Useless

--- client/src/routes/user.ts
+++ client/src/routes/user.ts
@@ -4,27 +4,27 @@
  * Description: Routes that a user can use.
  */
 
-import { RouteEntry } from "./index";
-import { AppFrame } from "../common/components";
-import UserTbale1 from '../features/adminPage/UserTable1';
-import { UserRole } from "../services/serverApi";
-import { Home, DummyText } from "../features";
+import { RouteEntry } from './index';
+import { AppFrame } from '../common/components';
+import UserTable1 from '../features/adminPage/UserTable1';
+import { UserRole } from '../services/serverApi';
+import { Home, DummyText } from '../features';
 
 export const userRoutes: RouteEntry[] = [
   {
-    path: "/",
+    path: '/',
     Component: AppFrame,
     children: [
       {
-        path: "/",
+        path: '/',
         Component: DummyText,
         exact: true,
       },
       {
-        path: "/table",
-        Component: UserTbale1,
+        path: '/table',
+        Component: UserTable1,
         exact: true,
-      }
+      },
     ],
   },
 ];
