3cdcddceefd56d533f2481f39b7af09902838d90
michael <skyjoy.liu@mail.utoronto.ca>
2020-10-30 00:02:30 -0700

    Complete admin main page

--- client/src/features/AdminHome/AdminHome.jsx
+++ client/src/features/AdminHome/AdminHome.jsx
@@ -1,6 +1,8 @@
 import React, { Component, useEffect } from 'react';
 // import { useHistory } from '../../hooks/useHistory';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import './style.css';
 
 // const history = useHistory();
 
@@ -11,9 +13,25 @@ import { Link } from 'react-router-dom';
 const AdminHome = () => {
   return (
     <div>
-      <Link to={'./../my/table'}>
-        {/* Using the global state variable from App.js */}
-        <button>go to</button>
+      <Link to={'./../my/cranesTable'}>
+        <Button className="button" variant="contained">
+          cranes
+        </Button>
+      </Link>
+      <Link to={'./../my/usersTable'}>
+        <Button className="button" variant="contained">
+          users
+        </Button>
+      </Link>
+      <Link to={'./../my/logTable'}>
+        <Button className="button" variant="contained">
+          activity log
+        </Button>
+      </Link>
+      <Link to={'./../my/storeTable'}>
+        <Button className="button" variant="contained">
+          store
+        </Button>
       </Link>
     </div>
   );
