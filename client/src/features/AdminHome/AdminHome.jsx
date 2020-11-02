2d95c397e62c52e96fde820d46a468e9bfb05fc1
michael <skyjoy.liu@mail.utoronto.ca>
2020-11-02 01:47:02 -0800

    Polish admin home page

--- client/src/features/AdminHome/AdminHome.jsx
+++ client/src/features/AdminHome/AdminHome.jsx
@@ -1,8 +1,8 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect } from "react";
 // import { useHistory } from '../../hooks/useHistory';
-import { Link } from '../../common';
-import Button from '@material-ui/core/Button';
-import './style.css';
+import { Link } from "../../common";
+import Button from "@material-ui/core/Button";
+import "./style.css";
 
 // const history = useHistory();
 
@@ -13,22 +13,22 @@ import './style.css';
 const AdminHome = () => {
   return (
     <div>
-      <Link to={'./../my/cranesTable'}>
+      <Link to={"./../my/cranesTable"}>
         <Button className="button" variant="contained">
           cranes
         </Button>
       </Link>
-      <Link to={'./../my/usersTable'}>
+      <Link to={"./../my/usersTable"}>
         <Button className="button" variant="contained">
           users
         </Button>
       </Link>
-      <Link to={'./../my/logTable'}>
+      <Link to={"./../my/logTable"}>
         <Button className="button" variant="contained">
           activity log
         </Button>
       </Link>
-      <Link to={'./../my/storeTable'}>
+      <Link to={"./../my/storeTable"}>
         <Button className="button" variant="contained">
           store
         </Button>
