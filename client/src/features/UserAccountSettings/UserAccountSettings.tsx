6d6c4264c11d30dc4d051a993a23af0050e81e20
Jimmy <jimmylanbcn@gmail.com>
2020-11-03 14:02:24 -0500

    Add account settings

---
+++ client/src/features/UserAccountSettings/UserAccountSettings.tsx
@@ -0,0 +1,16 @@
+/**
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-03
+ * Description: Account settings page for users using the app.
+ */
+import React, { FunctionComponent } from "react";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const UserAccountSettings: FunctionComponent<Props> = (props) => {
+  return <div>Account settings</div>;
+};
+
+export { UserAccountSettings };
