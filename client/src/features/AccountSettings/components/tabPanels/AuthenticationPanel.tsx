f70d2ea199be7e08d2fc35393513fabe0386553e
Jimmy <jimmylanbcn@gmail.com>
2020-11-04 19:16:53 -0500

    Update AuthenticationPanel.tsx with required fields

--- client/src/features/AccountSettings/components/tabPanels/AuthenticationPanel.tsx
+++ client/src/features/AccountSettings/components/tabPanels/AuthenticationPanel.tsx
@@ -3,13 +3,47 @@
  * Creation Date: 2020-11-04
  */
 import React, { FunctionComponent } from "react";
+import { useStyles } from "./tabPanels.style";
+import { FormDisabledEmailField } from "../FormDisabledEmailField/FormDisabledEmailField";
+import { FormHeader } from "../FormHeader/FormHeader";
+import { TextField } from "@material-ui/core";
+import { FormControlButtons } from "../FormControlButtons/FormControlButtons";
 
 interface OwnProps {}
 
 type Props = OwnProps;
 
 const AuthenticationPanel: FunctionComponent<Props> = (props) => {
-  return <div>Authentication panel</div>;
+  const classes = useStyles();
+
+  return (
+    <form autoComplete="off" className={classes.root}>
+      <FormHeader title="Update Credentials" className={classes.formHeader} />
+      <FormDisabledEmailField hideButton className={classes.formField} />
+      <TextField
+        variant="outlined"
+        label="Old Password"
+        type="password"
+        className={classes.formField}
+        fullWidth
+      />
+      <TextField
+        variant="outlined"
+        label="New Password"
+        type="password"
+        className={classes.formField}
+        fullWidth
+      />
+      <TextField
+        variant="outlined"
+        label="Confirm New Password"
+        type="password"
+        className={classes.formField}
+        fullWidth
+      />
+      <FormControlButtons primaryText="Save" secondaryText="Cancel" />
+    </form>
+  );
 };
 
 export { AuthenticationPanel };
