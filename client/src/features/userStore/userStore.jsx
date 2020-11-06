917fdc29f7b1f233b35c2e8c5f5334919ba37a48
michael <skyjoy.liu@mail.utoronto.ca>
2020-11-05 18:37:07 -0800

    Use fakeAPI in userStore

--- client/src/features/userStore/userStore.jsx
+++ client/src/features/userStore/userStore.jsx
@@ -1,124 +1,21 @@
-import React, { Component, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { Button } from "@material-ui/core";
 import { myStyles } from "./storeStyle";
 import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import UserStoreItem from "./UserStoreItem";
+import { loadStoreContents } from "../../services/serverApi/userStoreApi";
+import { uid } from "uid";
+// import { loadStoreContents } from "../../services/serverApi/userStoreApi";
 
 const userStore = () => {
   const classes = myStyles();
+
   return (
     <div>
-      <Paper elevation={2} className={classes.paper}>
-        <Grid container direction="row" spacing={2}>
-          <Grid item className="icon">
-            <SendOutlinedIcon className={classes.blue}></SendOutlinedIcon>
-          </Grid>
-          <Grid item xs sm container>
-            <Grid item xs container direction="column">
-              <Grid item xs container direction="column">
-                <Grid item>
-                  <Typography gutterBottom variant="h6">
-                    Blue Colored Cranes
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="p">
-                    Chill with a blue colored crane after a busy day!
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">500 Coins</Typography>
-              <Button className={classes.button}>Purchase</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper elevation={2} className={classes.paper}>
-        <Grid container direction="row" spacing={2}>
-          <Grid item className="icon">
-            <SendOutlinedIcon className={classes.red}></SendOutlinedIcon>
-          </Grid>
-          <Grid item xs sm container>
-            <Grid item xs container direction="column">
-              <Grid item xs container direction="column">
-                <Grid item>
-                  <Typography gutterBottom variant="h6">
-                    Red Colored Cranes
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="p">
-                    Ignite your passion with a bright red paper crane!
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">500 Coins</Typography>
-              <Button className={classes.button}>Purchase</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper elevation={2} className={classes.paper}>
-        <Grid container direction="row" spacing={2}>
-          <Grid item className="icon">
-            <SendOutlinedIcon className={classes.green}></SendOutlinedIcon>
-          </Grid>
-          <Grid item xs sm container>
-            <Grid item xs container direction="column">
-              <Grid item xs container direction="column">
-                <Grid item>
-                  <Typography gutterBottom variant="h6">
-                    Green Colored Cranes
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="p">
-                    {" "}
-                    Feel refreshed using a lime green paper crane!
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">500 Coins</Typography>
-              <Button className={classes.button}>Purchase</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-      <Paper elevation={2} className={classes.paper}>
-        <Grid container direction="row" spacing={2}>
-          <Grid item className="icon">
-            <SendOutlinedIcon className={classes.yellow}></SendOutlinedIcon>
-          </Grid>
-          <Grid item xs sm container>
-            <Grid item xs container direction="column">
-              <Grid item xs container direction="column">
-                <Grid item>
-                  <Typography gutterBottom variant="h6">
-                    Yellow Colored Cranes
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="p">
-                    Spread your joy with a luminous yellow paper crane!
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">500 Coins</Typography>
-              <Button className={classes.button}>Purchase</Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
+      <UserStoreItem getData={loadStoreContents} />
     </div>
   );
 };
---
+++ client/src/services/serverApi/userStoreApi.ts
@@ -0,0 +1,35 @@
+import { getFakeServerCall } from "./helpers";
+import { ServerResponse, UserRole } from "./models";
+
+export const loadStoreContents = () => {
+  const response: ServerResponse = {
+    success: true,
+    data: [
+      {
+        itemName: "Blue Colored Cranes",
+        itemDesc: "Chill with a blue colored crane admist a busy day!",
+        price: "500 Coins",
+        color: "blue",
+      },
+      {
+        itemName: "Red Colored Cranes",
+        itemDesc: "Ignite your passion with a bright red paper crane!",
+        price: "500 Coins",
+        color: "red",
+      },
+      {
+        itemName: " Green Colored Cranes",
+        itemDesc: "Feel refreshed using a lime green paper crane!",
+        price: "500 Coins",
+        color: "green",
+      },
+      {
+        itemName: "Yellow Colored Cranes",
+        itemDesc: "Spread your joy with a luminous yellow paper crane!",
+        price: "500 Coins",
+        color: "yellow",
+      },
+    ],
+  };
+  return getFakeServerCall(response, 0.5);
+};
