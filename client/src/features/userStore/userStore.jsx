81ffcda25a92b36d4ecc0a0d4fe43dc50d24bb3c
michael <skyjoy.liu@mail.utoronto.ca>
2020-11-08 03:19:51 -0800

    Delete useless imports

--- client/src/features/userStore/userStore.jsx
+++ client/src/features/userStore/userStore.jsx
@@ -1,14 +1,7 @@
-import React, { useRef, useEffect } from "react";
-import { Button } from "@material-ui/core";
+import React from "react";
 import { myStyles } from "./storeStyle";
-import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
 import UserStoreItem from "./UserStoreItem";
 import { loadStoreContents } from "../../services/serverApi/userStoreApi";
-
-// import { loadStoreContents } from "../../services/serverApi/userStoreApi";
 
 const userStore = () => {
   const classes = myStyles();
