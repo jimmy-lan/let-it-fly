73054dad5853fe26b0ee6331e56272fd1751c034
Jimmy <jimmylanbcn@gmail.com>
2020-11-09 21:35:26 -0500

    Add UserFriendsPage.tsx

---
+++ client/src/features/UserFriendsPage/UserFriendsPage.tsx
@@ -0,0 +1,15 @@
+/**
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-09
+ */
+import React, { FunctionComponent } from "react";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const UserFriendsPage: FunctionComponent<Props> = (props) => {
+  return <div>Friends</div>;
+};
+
+export { UserFriendsPage };
