8f8bea4d50af116b056007823354d296f91f0e3c
Jimmy <jimmylanbcn@gmail.com>
2020-11-30 21:05:39 -0500

    Index files

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Initial setup");
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-30
+ */
+
+export * from "./models";
+export * from "./middlewares";
+export * from "./middlewares";
---
+++ common/src/middlewares/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-30
+ */
+
+export * from "./extractUser";
+export * from "./handleErrors";
+export * from "./requireAuth";
+export * from "./validateRequest";
