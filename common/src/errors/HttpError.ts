2be57365f05d6a2e9435804129344ec2eefd7953
Jimmy <jimmylanbcn@gmail.com>
2020-11-30 19:41:30 -0500

    Add HttpError.ts

---
+++ common/src/errors/HttpError.ts
@@ -0,0 +1,28 @@
+/**
+ * This class represents an HttpError with an http status code
+ * and a list of error messages.
+ * Error messages returned by serializeErrors method should
+ * meant to be sent to clients.
+ * Standard message property of the Error class is used for
+ * logging purposes only.
+ *
+ * @see Error
+ * @version 1.0
+ */
+export abstract class HttpError extends Error {
+  abstract statusCode: number;
+
+  /**
+   * Construct HttpError
+   * @param message Error message for logging purposes
+   */
+  protected constructor(message?: string) {
+    super(message);
+
+    // Configure this class to work with typescript
+    // Need this statement when extending a typescript built-in class
+    Object.setPrototypeOf(this, HttpError.prototype);
+  }
+
+  abstract serializeErrors(): { message: string; cause?: string }[];
+}
