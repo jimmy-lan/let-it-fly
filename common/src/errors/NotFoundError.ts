02eac4dd83951052fa0459e1138d427dfc6cb432
Jimmy <jimmylanbcn@gmail.com>
2020-11-30 19:44:36 -0500

    Add NotFoundError.ts

---
+++ common/src/errors/NotFoundError.ts
@@ -0,0 +1,20 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-11-30
+ */
+
+import { HttpError } from "./HttpError";
+
+export class NotFoundError extends HttpError {
+  statusCode = 404;
+
+  constructor(message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
+}
