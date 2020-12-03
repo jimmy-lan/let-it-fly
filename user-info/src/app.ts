ac14c2cf23527079ba85284aaafae92f5a178db5
Jimmy <jimmylanbcn@gmail.com>
2020-12-03 18:42:29 -0500

    Add structure

---
+++ user-info/src/app.ts
@@ -0,0 +1,33 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-01
+ */
+
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+import { NotFoundError, handleErrors } from "@ly-letitfly/common";
+// import * as routes from "./routes";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+// app.use("/api/auth", Object.values(routes));
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(handleErrors);
+
+export { app };
