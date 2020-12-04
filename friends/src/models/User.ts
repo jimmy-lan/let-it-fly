299f1078e84dd94e9b8fd774f2879f4e1243a586
Jimmy <jimmylanbcn@gmail.com>
2020-12-04 12:49:37 -0500

    Add User model

---
+++ friends/src/models/User.ts
@@ -0,0 +1,46 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-04
+ */
+
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+interface UserProps {
+  id: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+}
+
+interface UserDocument extends Document {
+  firstName: string;
+  lastName: string;
+  avatar: string;
+}
+
+interface UserModel extends Model<UserDocument> {
+  build(props: UserProps): UserDocument;
+}
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  avatar: String,
+});
+
+const build = (props: UserProps) => {
+  const user = { _id: props.id, ...props };
+  delete user.id;
+  return new User(props);
+};
+userSchema.static("build", build);
+
+const User = mongoose.model<UserDocument, UserModel>("User", userSchema);
+
+export { User };
