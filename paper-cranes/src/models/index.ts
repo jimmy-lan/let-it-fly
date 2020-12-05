0dc9782ad8b52d15fef8f90f88a331a18a430817
Jimmy <jimmylanbcn@gmail.com>
2020-12-05 18:55:20 -0500

    Add index.ts

---
+++ paper-cranes/src/models/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-05
+ */
+
+export * from "./PaperCrane";
+export * from "./PaperCraneRecord";
+export * from "./Reply";
+export * from "./User";
