90954a7834c81701acb084d783636238ef8546e0
Jimmy <jimmylanbcn@gmail.com>
2020-12-05 17:51:30 -0500

    Add user model

--- paper-cranes/src/models/User.ts
+++ paper-cranes/src/models/User.ts
@@ -3,8 +3,52 @@
  * Creation Date: 2020-12-05
  */
 
+import mongoose, { Schema, Document, Model } from "mongoose";
+
 interface UserProps {
   id: string;
   firstName: string;
   lastName: string;
 }
+
+export interface UserDocument extends Document {
+  firstName: string;
+  lastName: string;
+}
+
+interface UserModel extends Model<UserDocument> {
+  build(props: UserProps): UserDocument;
+}
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+const build = (props: UserProps) => {
+  const user = { _id: props.id, ...props };
+  delete user.id;
+  return new User(user);
+};
+userSchema.static("build", build);
+
+const User = mongoose.model<UserDocument, UserModel>("User", userSchema);
+
+export { User };
