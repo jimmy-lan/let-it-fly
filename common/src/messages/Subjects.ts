2e787388668c7e30871bbf0665dd6568e0c6b71d
Jimmy <jimmylanbcn@gmail.com>
2020-12-06 13:39:58 -0500

    Add UserInfoUpdate.ts

--- common/src/messages/Subjects.ts
+++ common/src/messages/Subjects.ts
@@ -12,4 +12,5 @@ export enum Subjects {
   AccountSignUp = "account:signup",
   AccountSignIn = "account:signin",
   AccountUserRoleUpdate = "account:user-role-update",
+  UserInfoUpdate = "user-info:update",
 }
