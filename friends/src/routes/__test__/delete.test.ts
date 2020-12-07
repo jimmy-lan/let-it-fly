d5799ae2db9bf7c91874c891bdc1522ce4f943e9
Jimmy <jimmylanbcn@gmail.com>
2020-12-07 17:28:56 -0500

    Update delete test

--- friends/src/routes/__test__/delete.test.ts
+++ friends/src/routes/__test__/delete.test.ts
@@ -65,6 +65,48 @@ it("deletes friend on valid request", async () => {
 
   expect(response.body.success).toBeTruthy();
 
-  const newFriendRelation = await Friend.find({ user: fakeUser.id });
-  expect(newFriendRelation.length).toBe(0);
+  const newFriendRelation = await Friend.findOne({ user: fakeUser.id });
+  expect(newFriendRelation!.friends.length).toBe(0);
+});
+
+it("deletes friend on valid request", async () => {
+  const fakeUser = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: "user@user.com",
+    role: UserRole.user,
+  };
+  const fakeUserFriend1 = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: "friend@friend.com",
+    role: UserRole.user,
+  };
+  const fakeUserFriend2 = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    email: "friend@friend.com",
+    role: UserRole.user,
+  };
+
+  const user = User.build({ id: fakeUser.id });
+  await user.save();
+  const friend1 = User.build({ id: fakeUserFriend1.id });
+  await friend1.save();
+  const friend2 = User.build({ id: fakeUserFriend2.id });
+  await friend2.save();
+  const friendRelation = Friend.build({
+    user: user.id,
+    friends: [friend1, friend2],
+  });
+  await friendRelation.save();
+
+  const response = await request(app)
+    .delete("/api/friends/" + fakeUserFriend2.id)
+    .set("Cookie", global.getTestCookie(fakeUser))
+    .send()
+    .expect(204);
+
+  expect(response.body.success).toBeTruthy();
+
+  const newFriendRelation = await Friend.findOne({ user: fakeUser.id });
+  expect(newFriendRelation!.friends.length).toBe(1);
+  expect(newFriendRelation!.friends[0].id).toEqual(fakeUserFriend1.id);
 });
