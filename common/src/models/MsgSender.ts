820981561ca864de24e0cfb4c99bc89bd2144f0c
Jimmy <jimmylanbcn@gmail.com>
2020-12-06 23:17:39 -0500

    Update

--- common/src/models/MsgSender.ts
+++ common/src/models/MsgSender.ts
@@ -2,7 +2,7 @@
  * Created by Jimmy Lan
  * Creation Date: 2020-12-02
  * Description:
- *     A message sender to send a particular
+ *     A message senders to send a particular
  *     type of message through NATS streaming.
  */
 
