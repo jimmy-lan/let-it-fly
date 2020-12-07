16f12b2c8176009aa70d0d134509d68614b5be50
Jimmy <jimmylanbcn@gmail.com>
2020-12-07 13:25:48 -0500

    Update service logic on signup

--- friends/src/messages/receivers/AccountSignUpMsgReceiver.ts
+++ friends/src/messages/receivers/AccountSignUpMsgReceiver.ts
@@ -6,7 +6,7 @@
 import { Message } from "node-nats-streaming";
 import { AccountSignUp, MsgReceiver, Subjects } from "@ly-letitfly/common";
 import { queueGroup } from "./constants";
-import { User } from "../../models";
+import { Friend, User } from "../../models";
 
 export class AccountSignUpMsgReceiver extends MsgReceiver<AccountSignUp> {
   subject: Subjects.AccountSignUp = Subjects.AccountSignUp;
@@ -25,7 +25,14 @@ export class AccountSignUpMsgReceiver extends MsgReceiver<AccountSignUp> {
       await user.save();
     } catch (error) {
       console.error(error);
+      // Acknowledge because this error is most likely caused by
+      // concurrency
+      msg.ack();
+      return;
     }
+
+    const friendRelation = Friend.build({ user: user, friends: [] });
+    await friendRelation.save();
 
     msg.ack();
   }
