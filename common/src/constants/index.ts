6b9a84923ed14420f3d9a54c0cbf595a7c1f9654
Jimmy <jimmylanbcn@gmail.com>
2020-12-08 17:32:42 -0500

    Update constants in the common package

--- common/src/constants/index.ts
+++ common/src/constants/index.ts
@@ -3,7 +3,28 @@
  * Creation Date: 2020-12-05
  */
 
-export const defaultUserProperties = {
-  paperCraneStyles: ["#bdbdbd"],
+import { StoreItemCategory } from "../enums";
+
+export interface PaperCraneStyleItem {
+  name: string;
+  category: StoreItemCategory;
+  description: string;
+  price: number;
+  value: string;
+}
+
+export const defaultUserProperties: {
+  paperCraneStyleItems: PaperCraneStyleItem[];
+  coins: number;
+} = {
+  paperCraneStyleItems: [
+    {
+      name: "Default Style",
+      category: StoreItemCategory.paperCraneStyle,
+      description: "Default paper crane style.",
+      price: 0,
+      value: "#bdbdbd",
+    },
+  ],
   coins: 1000,
 };
