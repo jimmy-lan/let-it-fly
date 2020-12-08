784c87a925a58c0a6d751566c73b13af14d2cdfd
Jimmy <jimmylanbcn@gmail.com>
2020-12-08 12:07:17 -0500

    Implement patch route

--- paper-cranes/src/routes/patch.ts
+++ paper-cranes/src/routes/patch.ts
@@ -3,11 +3,62 @@
  * Creation Date: 2020-12-07
  */
 
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
+import { body, param } from "express-validator";
 import mongoose from "mongoose";
+import {
+  BadRequestError,
+  ForbiddenError,
+  validateRequest,
+} from "@ly-letitfly/common";
+import { PaperCrane, PaperCraneRecord } from "../models";
 
 const router = express.Router();
 
-router.patch("/:id/info", async (req: Request, res: Response) => {});
+router.patch(
+  "/:paperCraneId/info",
+  [
+    param("paperCraneId")
+      .custom((id: string) => mongoose.Types.ObjectId.isValid(id))
+      .withMessage("Paper crane id must be valid"),
+    body("isStarred").optional().isBoolean(),
+    body("isUnread").optional().isBoolean(),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { isStarred, isUnread } = req.body;
+    const { paperCraneId } = req.params;
+    const userId = req.user!.id;
+
+    // Find paper crane record for this user
+    const paperCrane = await PaperCrane.findById(paperCraneId);
+    if (!paperCrane) {
+      throw new BadRequestError(`Paper crane ${paperCraneId} does not exist`);
+    }
+
+    const record = await PaperCraneRecord.findOne({ userId, paperCrane });
+    if (!record) {
+      throw new ForbiddenError();
+    }
+
+    // Update record
+    if (isStarred !== undefined) {
+      record.isStarred = isStarred;
+    }
+    if (isUnread !== undefined) {
+      record.isUnread = isUnread;
+    }
+    await record.save();
+
+    return res.send({
+      success: true,
+      data: {
+        id: paperCrane.id,
+        isStarred: record.isStarred,
+        isUnread: record.isUnread,
+      },
+    });
+  }
+);
 
 export { router as patchRouter };
