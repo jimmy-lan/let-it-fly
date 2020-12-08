3f107ef5fa9ee0f61bea0cc665d658cc94fc72ac
Jimmy <jimmylanbcn@gmail.com>
2020-12-07 22:13:35 -0500

    Add patch.ts

---
+++ paper-cranes/src/routes/patch.ts
@@ -0,0 +1,11 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-07
+ */
+
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+router.patch("/:id", async (req: Request, res: Response) => {});
