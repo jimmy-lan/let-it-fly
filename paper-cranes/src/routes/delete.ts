fd13fcb9a1ed3f80400c232387ec42d0b1512255
Jimmy <jimmylanbcn@gmail.com>
2020-12-07 23:01:58 -0500

    Implement delete route

--- paper-cranes/src/routes/delete.ts
+++ paper-cranes/src/routes/delete.ts
@@ -3,11 +3,43 @@
  * Creation Date: 2020-12-07
  */
 
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
+import { param } from "express-validator";
 import mongoose from "mongoose";
+import { BadRequestError, validateRequest } from "@ly-letitfly/common";
+import { PaperCraneRecord } from "../models";
 
 const router = express.Router();
 
-router.delete("/:id", async (req: Request, res: Response) => {});
+router.delete(
+  "/:paperCraneId",
+  [
+    param("paperCraneId")
+      .custom((id: string) => mongoose.Types.ObjectId.isValid(id))
+      .withMessage("Id must be valid."),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { paperCraneId } = req.params;
+    const userId = req.user!.id;
+
+    // Make sure the current user can delete this paper crane
+    const paperCraneRecord = await PaperCraneRecord.findOne({
+      userId,
+    }).populate("paperCrane.id");
+    if (!paperCraneRecord) {
+      // User does not have any paper crane
+      throw new BadRequestError(
+        `Cannot delete paper crane with id value ${paperCraneId}`
+      );
+    }
+
+    // Soft delete
+    paperCraneRecord.isDeleted = true;
+    await paperCraneRecord.save();
+
+    return res.send({ success: true, data: paperCraneRecord.paperCrane.id });
+  }
+);
 
 export { router as deleteRouter };
