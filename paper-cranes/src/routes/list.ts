fc9961c1d6469ce0dd2c42ff10c61ec27ea0e440
Jimmy <jimmylanbcn@gmail.com>
2020-12-07 22:11:36 -0500

    Add list.ts

---
+++ paper-cranes/src/routes/list.ts
@@ -0,0 +1,15 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2020-12-07
+ */
+
+import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+router.get("/sent", async (req: Request, res: Response) => {});
+
+router.get("/received", async (req: Request, res: Response) => {});
+
+router.get("/unread", async (req: Request, res: Response) => {});
