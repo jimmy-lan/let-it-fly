c7bceacdfb060a0108821eabee8bf6aaa29a720e
michael <skyjoy.liu@mail.utoronto.ca>
2020-12-11 14:15:16 -0800

    Add axios request to user store

--- client/src/services/serverApi/userStoreApi.ts
+++ client/src/services/serverApi/userStoreApi.ts
@@ -1,35 +1,24 @@
 import { getFakeServerCall } from "./helpers";
 import { ServerResponse } from "./models";
-
+import axios from "axios";
 export const loadStoreContents = () => {
-  const response: ServerResponse = {
-    success: true,
-    data: [
-      {
-        itemName: "Blue Colored Cranes",
-        itemDesc: "Chill with a blue colored crane admist a busy day!",
-        price: "500 Coins",
-        color: "blue",
-      },
-      {
-        itemName: "Red Colored Cranes",
-        itemDesc: "Ignite your passion with a bright red paper crane!",
-        price: "500 Coins",
-        color: "red",
-      },
-      {
-        itemName: " Green Colored Cranes",
-        itemDesc: "Feel refreshed using a lime green paper crane!",
-        price: "500 Coins",
-        color: "green",
-      },
-      {
-        itemName: "Yellow Colored Cranes",
-        itemDesc: "Spread your joy with a luminous yellow paper crane!",
-        price: "500 Coins",
-        color: "yellow",
-      },
-    ],
-  };
-  return getFakeServerCall(response, 0.5);
+  let response: ServerResponse;
+  const url = "https://letitfly.dev/api/users/property/inventory";
+  axios
+    .get(url)
+    .then((result) => {
+      if ("data" in result.data) {
+        response = {
+          success: result.data.success,
+          data: result.data.data,
+        };
+      } else if ("error" in result.data) {
+        console.log(result.data.error);
+      }
+
+      return getFakeServerCall(response, 0.5);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
