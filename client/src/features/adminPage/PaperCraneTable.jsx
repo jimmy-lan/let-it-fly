ae10d8252b4db146f46dd8df5ac1af8dae1f433e
brady <bradyhuai@outlook.com>
2020-12-11 14:58:05 -0800

    Implement paper crance api

--- client/src/features/adminPage/PaperCraneTable.jsx
+++ client/src/features/adminPage/PaperCraneTable.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import Table from "./CraneExpandable";
 import { loadPaperCraneTable } from "../../services/serverApi/adminApi";
+
+const loadPaperCraneTable = () => {
+  return axios.get("/api/paper-cranes").then(
+      res => {
+          const { body } = res.data;
+          return {
+              "data":
+                  body.map(paper => {
+                      return {
+                          ...paper,
+                          "title": paper.title,
+                          "style": paper.style,
+                          "content": paper.content,
+                          "from": paper.senderId,
+                      }
+                  })
+          };
+      }
+  );
+};
+
 const PaperCraneTable = () => (
   <Table
     columns={[
